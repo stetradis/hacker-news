f557eb5b4425d437edc0b2f66d05d2f827dc0c51
Stella Tetradis <stella-lev@hotmail.com>
2020-10-24 10:06:11 -0400

    Config fixes

--- HackerNews/Startup.cs
+++ HackerNews/Startup.cs
@@ -35,15 +35,7 @@ namespace HackerNews
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
